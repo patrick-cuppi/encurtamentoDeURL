1ba3c37b71fbf02d043be41f4b9b4fdac164be9f
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-03-20 17:14:12 -0300

    feat: method GET created

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import fastify from "fastify"
 import { z } from 'zod'
 import { sql } from "./lib/postgres"
+import postgres from "postgres"
 
 const app = fastify()
+
+app.get('/links', async () => {
+    const result = await sql/*sql*/`
+    SELECT *
+    FROM short_links
+    ORDER BY created_at DESC    
+    `
+
+    return result
+})
 
 app.post('/links', async (request, reply) => {
     const createLinkSchema = z.object({
@@ -12,15 +23,29 @@ app.post('/links', async (request, reply) => {
     
     const { code, url } = createLinkSchema.parse(request.body)
 
+    try {    
     const result = await sql/*sql*/`
-        INSERT INTO short_links (code, original_url)
-        VALUES (${code}, ${url})
-        RETURNING id 
-    `
+    INSERT INTO short_links (code, original_url)
+    VALUES (${code}, ${url})
+    RETURNING id 
+`
 
     const link = result[0]
 
     return reply.status(201).send({shortLinkId: link.id})
+
+} catch (err) {
+    if (err instanceof postgres.PostgresError) {
+        if (err.code === '23505') {
+            return reply.status(400).send({message: 'Duplicated code!'})
+        }
+    }
+
+    console.error(err)
+
+    return reply.status(500).send({message: 'Internal error!'})
+}
+
 })
 
 app.listen({ 
